055501d41579f47b4edfa0daaea38789b158821a
matgregorio <mateusgregorio178@gmail.com>
2021-09-09 23:15:12 -0300

    edicao

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const path = require('path')
 
 app.use(express.static(__dirname + '/public'));
 
-app.use('/build/', express.static(path.join(__dirname, 'node_modules/three/build')));
+app.use('/build/', express.static(path.join(__dirname, 'three.js/build')));
 
-app.use('/jsm/', express.static(path.join(__dirname, 'node_modules/three/jsm')));
+app.use('/jsm/', express.static(path.join(__dirname, 'three.js/examples/jsm')));
 
 app.listen(3000, () => console.log('Visit http://127.0.0.1:3000'));
 
